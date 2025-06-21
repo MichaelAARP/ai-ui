8d9204174e670985b15987d8968809bea1a9aae6
MichaelAARP <mpabst@aarp.org>
2025-06-21 12:17:43 -0400

    added iframe to UI.

--- care-chat/src/main.tsx
+++ care-chat/src/main.tsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import { ChainlitContext, ChainlitAPI } from '@chainlit/react-client';
-import { RecoilRoot } from 'recoil';
 import { BrowserRouter } from 'react-router-dom';
-
-const CHAINLIT_SERVER_URL = 'https://demchat.agetechcollaborative.org';
-
-const apiClient = new ChainlitAPI(CHAINLIT_SERVER_URL, 'webapp');
+import App from './App';
+import './index.css'; // optional
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChainlitContext.Provider value={apiClient}>
-      <RecoilRoot>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </RecoilRoot>
-    </ChainlitContext.Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import { ChainlitContext, ChainlitAPI } from '@chainlit/react-client';
+// import { RecoilRoot } from 'recoil';
+// import { BrowserRouter } from 'react-router-dom';
+
+// const CHAINLIT_SERVER_URL = 'https://demchat.agetechcollaborative.org';
+
+// const apiClient = new ChainlitAPI(CHAINLIT_SERVER_URL, 'webapp');
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <ChainlitContext.Provider value={apiClient}>
+//       <RecoilRoot>
+//         <BrowserRouter>
+//           <App />
+//         </BrowserRouter>
+//       </RecoilRoot>
+//     </ChainlitContext.Provider>
+//   </React.StrictMode>
+// );
